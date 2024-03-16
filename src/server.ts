a2bdb1aeca00d4be6f89b1fce55fae64fd079ae0
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-16 16:11:17 -0300

    feat: att server routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import fastify, { FastifyInstance } from "fastify";
+import { userRoutes } from "./routes/user.routes";
 
 const app: FastifyInstance = fastify()
 // basicamente o logger da varias informacoes no terminal
+app.register(userRoutes, {
+    prefix: '/users',
+})
+
 app.listen({
     port:3000,},
      ()=> console.log('Server is running on port 3000')
