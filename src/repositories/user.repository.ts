dfe9d1e5fbefbd2270eed9b87accf62fe0e9d768
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-16 16:12:05 -0300

    feat: incorporando database

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,15 @@
+import { prisma } from "../database/prisma-client";
 import { User, UserCreate, UserRepository } from "../interfaces/user.interface";
 
 class UserRepositoryPrisma implements UserRepository{
  async create(data: UserCreate): Promise<User> {
-    
+    const result  =  await prisma.user.create({
+        data:{
+            name: data.name,
+            email: data.email
+        }
+    })
+    return result
 }
 }
 export {UserRepositoryPrisma}
