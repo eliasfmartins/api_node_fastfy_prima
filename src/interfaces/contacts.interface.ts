955cc1cc5e504d4e6bc20333ac132165b191ff79
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-17 10:17:19 -0300

    feat: updating interfaces

--- src/interfaces/contacts.interface.ts
+++ src/interfaces/contacts.interface.ts
@@ -6,12 +6,19 @@ export interface Contact {
     userId: string;
 }
 export interface ContactCreate{
-    id: string;
+    name: string;
+    email: string;
+    phone: string;
+    userEmail: string;
+}
+export interface ContactCreateData {
     name: string;
     email: string;
     phone: string;
     userId: string;
 }
 export interface ContactRepository{
-    create(data: ContactCreate): Promise<Contact>
+    create(data: ContactCreateData): Promise<Contact>;
+    findByEmailOrPhone(email:string, phone:string): Promise<Contact|null>;
+    findAllContacts(userI: string): Promise<Contact[]>
 }
