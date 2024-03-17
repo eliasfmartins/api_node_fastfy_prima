ae3989510996af0854288cfabf6b0389939c2089
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-17 10:17:57 -0300

    feat: implemtando middleware

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,15 +1,27 @@
 import { FastifyInstance } from "fastify";
 import { ContactUseCase } from "../usecases/contact.usecase";
+import { ContactCreate } from "../interfaces/contacts.interface";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 export async function contactsRoutes(fastify: FastifyInstance){
     const contactUseCase = new ContactUseCase()
-    fastify.post<{Body: UserCreate}>('/',async (req, reply)=>{
-        const {name, email} = req.body
+    fastify.addHook('preHandler', authMiddleware)
+    fastify.post<{Body: ContactCreate}>('/',async (req, reply)=>{
+        const {name, email,phone} = req.body;
+        const emailUser =req.headers['email']
         try{
-            const data =  await contactUseCase.create({}
+            const data =  await contactUseCase.create({email,name,phone,userEmail: emailUser}
             );
             return reply.send(data)
         } catch(error){
+            reply.send(error)
+        }
+    })
+    fastify.get('/', async (req, reply)=>{
+        const emailUser = req.headers['email']
+        try{
+            const data = await contactUseCase.create
+        } catch (error) {
             reply.send(error)
         }
     })
